6c58e9d74030af172d0140e9d9c21ce94e51feae
Administrator <Administrator@zhl.local>
2026-06-17 12:17:51 +0200

    6

--- server.py
+++ server.py
@@ -789,7 +789,7 @@ def add_security_headers(response):
     nonce = getattr(g, "csp_nonce", secrets.token_urlsafe(16))
     response.headers["Content-Security-Policy"] = (
         f"default-src 'self'; "
-        f"script-src 'self' 'nonce-{nonce}' 'unsafe-inline'; "
+        f"script-src 'self' 'nonce-{nonce}'; "
         f"style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; "
         f"font-src 'self' https://fonts.gstatic.com; "
         f"connect-src 'self'; "
